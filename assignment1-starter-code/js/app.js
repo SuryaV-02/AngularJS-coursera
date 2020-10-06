ebfc2a382406c05e747ad3329ff17de21ae8734e
SuryaV-02 <66406502+SuryaV-02@users.noreply.github.com>
2020-10-06 18:15:12 +0530

    Update app.js

--- assignment1-starter-code/js/app.js
+++ assignment1-starter-code/js/app.js
@@ -13,7 +13,7 @@
 
 function splitter(){
 	var arr =$scope.inpt.split(',');
-	var output =document.getElementById('output');
+//	var output =document.getElementById('output');
 	var flag=0;
 	console.log(output);
 	for(let i=0;i<arr.length;++i){
@@ -22,15 +22,15 @@ function splitter(){
 		}
 	if(flag===0){
 		$scope.message="Please enter data first";
-		output.style.color="red";
+// 		output.style.color="red";
 	}
 	else if(flag<=3){
 		$scope.message="Enjoy!";
-		output.style.color="green";
+// 		output.style.color="green";
 	}
 	else{
 		$scope.message="Too much!";
-		output.style.color="green";
+// 		output.style.color="green";
 	}
 	}
 }
